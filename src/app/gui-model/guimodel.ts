71cb1bfd9a6ac55f9729fd6882f1fe0ee244597a
𝙎𝙖𝙣𝙟𝙞 𝙎𝙆 <sanjai.v@live.de>
2019-05-20 01:05:54 +0200

    updated

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,13 +6,10 @@
 
     private _guiModel = {
         "application": {
-<<<<<<< HEAD
+
             "title": "Sanjis Requirements Engineering Friend Tracker",
              "formList": [
-=======
-            "title": "Sanjais Requirements Engineering Friend Tracker",
-            "formList": [
->>>>>>> 167b09f37393c0b77b80abd3132a81697cbb880d
+
                 {
                     "id": "FriendForm",
                     "title": "Friend",
